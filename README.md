ead6d72fad2bdec235f6f2408b29c48a03551e81
joedoe <d_joe@gmx.net>
2017-12-18 15:37:50 +0100

    linefeed adjusted

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # code_library_scheme
-Basic console application for structuring content. Not for editing (done manually in .txt-file), just for viewing: either view table of content (command "toc") or an entry (enter corresponding number). The entries are all stored in a single .txt-file. Each entry needs to end with two empty lines as divider (except last one).
+Basic console application for structuring content. Not for editing (done manually in .txt-file), just for viewing: either view table of content (command "toc") or an entry (enter corresponding number). The entries are all stored in a single .txt-file. Each entry needs to end with three \#newlines as divider (except last one).
 ![screenshot](https://user-images.githubusercontent.com/26798159/33559901-8997982a-d90e-11e7-9299-48c3c444f283.JPG)
 ![screenshot](https://user-images.githubusercontent.com/26798159/33791173-51a5c568-dc88-11e7-862d-2a226b7cf5eb.JPG)
---
+++ code_library_scheme.rkt
@@ -0,0 +1,70 @@
+#lang racket
+
+(define file "library.txt")
+(define num 0)
+(define end-of-entry '(#\newline #\newline #\newline))
+(define eoe-length (length end-of-entry))
+(define lib '())
+
+;aux-function to increment global "num"
+(define (incr) (set! num (+ num 1)) num)
+
+;creates the association "lib" from the .txt-file "file" using the accumulator "acc"
+(define (create-lib)
+  (let ((port (open-input-file file)))
+    (let loop((char (read-char port)) (entry '()) (acc '()))
+      (cond ((eof-object? char)
+	    (close-input-port port)
+            (set! lib (append acc (list (cons (incr) (reverse entry))))))
+            ((and (>= (length entry) eoe-length) (equal? end-of-entry (list-tail (reverse entry) (- (length entry) eoe-length))))  
+             (loop (read-char port) '() (append acc (list (cons (incr) (reverse (list-tail entry eoe-length)))))))
+	  (else (loop (read-char port) (cons char entry) acc))))))
+
+;displays table of content
+(define (toc)
+  (display "----CODE LIBRARY SCHEME----\n")
+  (let loop-lib((lib lib))
+    (if (not (null? lib))
+        (let loop-entry((entry (car lib))(entry-title '()))
+          (if (eqv? (car entry) #\newline)
+              (begin
+                (display (car (reverse entry-title)))
+                (display " - ")
+                (display (list->string (cdr (reverse entry-title))))
+                (newline)
+                (loop-lib (cdr lib)))
+              (loop-entry (cdr entry) (cons (car entry) entry-title))))
+        (values))))
+
+;displays entry according to user input
+(define (display-entry input)
+  (newline)
+  (display (list->string (cdr (assv input lib)))))
+
+;validates user input
+(define (check-input input)
+  (cond((eqv? input 'toc)
+        (newline)
+        (toc)
+        (main))
+       ((not (number? input))
+        (display "\nType in a number or 'toc' for table of content.\n"))
+       ((or (> input num)(<= input 0))
+        (display "\nNot a valid number for any entry.\n"))
+       (else
+        (display-entry input)
+        (newline)
+        (main))))
+
+;main loop
+(define (main)
+  (cond((null? lib)
+          (create-lib)
+          (toc)
+          (main))
+       (else
+          (display "\nWhat would you like to read? ")
+          (check-input(read))
+          (main))))
+
+(main)
